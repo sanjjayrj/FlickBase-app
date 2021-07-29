289865f8f9950e4f797e0a4c41706c208c02d81a
Sanjay Raju <sanjjayrj@gmail.com>
2021-07-29 20:38:56 +0300

    aggregate and article pagination

--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -2,6 +2,7 @@ const express = require('express');
 let router = express.Router();
 const { checkLoggedIn } = require('../../middleware/auth');
 const { grantAccess } = require('../../middleware/roles');
+const { sortArgsHelper } = require('../../config/helpers')
 
 /// MODEL
 const { Article } = require('../../models/article_model')
@@ -70,4 +71,75 @@ router.route('/admin/:id')
         }
     })
 
+/// PAGINATION
+
+router.route("/admin/paginate")
+.post(checkLoggedIn, grantAccess('readAny','articles'), async(req,res)=>{
+    try {
+
+        // let aggQuery = Article.aggregate([
+        //     { $match: {status:"public"}},
+        //     { $match: { title:{ $regex:/some/ }}}
+        // ])
+
+        const limit = req.body.limit ? req.body.limit: 5;
+        const aggQuery = Article.aggregate();
+        const options = {
+            page: req.body.page,
+            limit,
+            sort:{_id:'desc'}
+        }
+
+        const articles = await Article.aggregatePaginate(aggQuery, options)
+        res.status(200).json(articles)
+    } catch(error){
+        console.log(error)
+        res.status(400).json({message:'Error',error})
+    }
+})
+
+
+/// NO AUTH REQUIRED
+
+router.route('/get_byid/:id')
+.get(async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const article = await Article.find({_id:_id,status:'public'});
+        if (!article || article.length === 0) {
+            return res.status(400).json({ message: "Article not found" })
+        }
+        res.status(200).json(article)
+    } catch(error){
+        res.status(400).json({message:"Error fetching article",error:error})
+    }
+})
+
+
+/// LOAD MORE
+
+router.route('/loadmore')
+.post(async(req,res)=>{
+    try {
+        let sortArgs = sortArgsHelper(req.body);
+
+        const articles = await Article
+        .find({status:'public'})
+        .sort([[sortArgs.sortBy,sortArgs.order]])
+        .skip(sortArgs.skip)
+        .limit(sortArgs.limit)
+
+        res.status(200).json(articles)
+    } catch(error){
+        res.status(400).json({ message: "Error fetching article", error: error })
+    }
+})
+
 module.exports = router;
+
+// {
+        //     "sortBy": "_id",
+        //     "order": "asc",
+        //     "limit": 10,
+        //     "skip": 0
+        // }
