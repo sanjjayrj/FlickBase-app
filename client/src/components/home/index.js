ded68e83e737ea0126f319ebddcd7534694e459b
Sanjay Raju <sanjjayrj@gmail.com>
2021-08-04 22:14:34 +0300

    redux and side drawer

---
+++ client/src/components/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div
+            style={{fontFamily:'Fredoka One'}}
+        >
+        home
+        </div>
+    )
+}
+
+export default Home;
---
+++ client/src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import GoogleFontLoader from 'react-google-font-loader';
+
+import Home from './components/home';
+import Header from './components/navigation/header';
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route path="/" component={Home}/>
+            </Switch>
+            <GoogleFontLoader
+                fonts={[
+                    { font: 'Roboto', weights: [300,400,900]},
+                    { font: 'Fredoka One'}
+                ]}
+            />
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
