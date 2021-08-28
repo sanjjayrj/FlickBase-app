2344db04b2098358fc32f726aeb8eb174181fb5d
Sanjay Raju <sanjjayrj@gmail.com>
2021-08-28 17:25:32 +0300

    WYSIWYG form

--- client/src/components/dashboard/articles/add.js
+++ client/src/components/dashboard/articles/add.js
@@ -1,8 +1,11 @@
-import React,{ useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import AdminLayout from '../../../hoc/adminLayout';
 import { useFormik, FieldArray, FormikProvider } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import { validation, formValues } from './validationSchema';
+
+import WYSIWYG from '../../../utils/forms/wysiwyg';
+
 import {
     TextField,
     Button,
@@ -20,31 +23,62 @@ import AddIcon from '@material-ui/icons/Add';
 
 
 const AddArticle = (props) => {
-
+    const [editorBlur, setEditorBlur] = useState(false);
+    const actorsValue = useRef('');
     const formik = useFormik({
         enableReinitialize: true,
         initialValues: formValues,
         validationSchema: validation,
-        onSubmit:(values,{resetForm}) => {
+        onSubmit: (values, { resetForm }) => {
             console.log(values)
         }
-    })
+    });
+
+    const handleEditorState = (state) => {
+        formik.setFieldValue('content', state, true);
+    }
+
+    const handleEditorBlur = (blur) => {
+        setEditorBlur(true);
+    }
+
     const errorHelper = (formik, values) => ({
         error: formik.errors[values] && formik.touched[values] ? true : false,
         helperText: formik.errors[values] && formik.touched[values] ? formik.errors[values] : null
     });
 
-    return(
-        <AdminLayout section="Add articlee">
+    return (
+        <AdminLayout section="Add article">
             <form className="mt-3 article_form" onSubmit={formik.handleSubmit}>
                 <div className="form-group">
                     <TextField
-                        style={{width:'100%'}}
+                        style={{ width: '100%' }}
                         name="title"
                         label="Enter a title"
                         variant="outlined"
                         {...formik.getFieldProps('title')}
-                        {...errorHelper(formik,'title')}
+                        {...errorHelper(formik, 'title')}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <WYSIWYG
+                        setEditorState={(state)=> handleEditorState(state)}
+                        setEditorBlur={(blur)=> handleEditorBlur(blur)}
+                    />
+
+                    {
+                        formik.errors.content && editorBlur ?
+                            <FormHelperText error={true}>
+                                {formik.errors.content}
+                            </FormHelperText>
+                            : null
+                    }
+
+                    <TextField
+                        type="hidden"
+                        name="content"
+                        {...formik.getFieldProps('content')}
                     />
                 </div>
 
@@ -62,7 +96,7 @@ const AddArticle = (props) => {
                 </div>
 
                 <Divider className="mt-3 mb-3" />
-                <h4>Movie Data and Score</h4>
+                <h5>Movie Data and Score</h5>
                 <div className="form-group">
                     <TextField
                         style={{ width: '100%' }}
@@ -74,7 +108,85 @@ const AddArticle = (props) => {
                     />
                 </div>
 
-                <Divider className="mt-3 mb-3"/>
+                <FormikProvider value={formik}>
+                    <h5>Add the actors:</h5>
+                    <FieldArray
+                        name="actors"
+                        render={arrayhelpers => (
+                            <div>
+                                <Paper className="actors_form">
+                                    <InputBase
+                                        inputRef={actorsValue}
+                                        className="input"
+                                        placeholder="Add actor name here"
+                                    />
+                                    <IconButton
+                                        onClick={()=>{
+                                            arrayhelpers.push(actorsValue.current.value)
+                                            actorsValue.current.value = ''
+                                        }}
+                                    >
+                                        <AddIcon />
+                                    </IconButton>
+                                </Paper>
+                                {
+                                    formik.errors.actors && formik.touched.actors ?
+                                        <FormHelperText error={true}>
+                                            {formik.errors.actors}
+                                        </FormHelperText>
+                                        : null
+                                }
+                                <div className="chip_container">
+                                    {formik.values.actors.map((actor, index) => (
+                                        <div key={index}>
+                                            <Chip
+                                                label={`${actor}`}
+                                                color="primary"
+                                                onDelete={()=> arrayhelpers.remove(index)}
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                    />
+                </FormikProvider>
+
+                <div className="form-group">
+                    <TextField
+                        style={{ width: '100%' }}
+                        name="director"
+                        label="Enter the director's name"
+                        variant="outlined"
+                        {...formik.getFieldProps('director')}
+                        {...errorHelper(formik, 'director')}
+                    />
+                </div>
+
+                <FormControl variant="outlined">
+                    <h5>Select a status</h5>
+                    <Select
+                        name="status"
+                        {...formik.getFieldProps('status')}
+                        error={formik.errors.status && formik.touched.status ? true : false}
+                    >
+                        <MenuItem value=''>
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value="draft">Draft</MenuItem>
+                        <MenuItem value="public">Public</MenuItem>
+
+                    </Select>
+                    {
+                        formik.errors.status && formik.touched.status ?
+                            <FormHelperText error={true}>
+                                {formik.errors.status}
+                            </FormHelperText>
+                            : null
+                    }
+                </FormControl>
+
+                <Divider className="mt-3 mb-3" />
                 <Button
                     variant="contained"
                     color="primary"
