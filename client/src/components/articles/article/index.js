367e2161a7d4766f6445c426cd751a96e36c50fb
Sanjay Raju <sanjjayrj@gmail.com>
2021-12-09 04:32:33 +0530

    redirect to watch movie

--- client/src/components/articles/article/index.js
+++ client/src/components/articles/article/index.js
@@ -1,3 +1,4 @@
+import { Button } from '@material-ui/core';
 import React,{ useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getArticle } from '../../../store/actions/article_actions';
@@ -17,6 +18,16 @@ const Article = (props) => {
             dispatch(clearCurrentArticle())
         }
     },[dispatch])
+    const followLink = (url) => {
+        url.replace("(", "%28")
+        url.replace(")", "%29")
+        url.replace(":", "%3A")
+        url.replace("!", "%21")
+        url.replace(" ", "%20")
+        url.replace("#", "%23")
+        const base = "https://www.justwatch.com/in/search?q="
+        window.open(base+url,"_blank")
+    }
 
     return(
         <>
@@ -26,6 +37,18 @@ const Article = (props) => {
                         <img alt="poster" src={current.bg_image.slice(0, 49) +"c_thumb,h_600,w_1200/" + current.bg_image.slice(49)}/>
                     </div>
                     <h1>{current.title}</h1>
+                    <div className="row col-md-4">
+                        <Button
+                            variant='contained'
+                            type='button'
+                            color='secondary'
+                            onClick={ 
+                                () => followLink(current.title)
+                            }
+                        >
+                            Watch {current.title}
+                        </Button>
+                    </div>
                     <div className="mt-3 content">
                         <div dangerouslySetInnerHTML={
                             { __html: current.content }
